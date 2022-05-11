aff91185baabe1362438130b7b24104dd1628d72
Maria Koutli <82814757+koutlm@users.noreply.github.com>
2022-05-11 11:29:59 +0300

    Feature1 (#1)
    * addition
    
    * plus5
    
    * plus10

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,4 +3,12 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
+
+    protected int add(int a, int b) {
+        return a + b;
+    }
+
+    protected int plus10(int a) {
+        return a + 10;
+    }
 }
