e72b6b454c98de3ad066ef70ee1c11708e433d68
Koutli Maria <maria.koutli@pfizer.com>
2022-05-11 11:20:41 +0300

    addition

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,4 +3,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
+
+    protected int add(int a, int b) {
+        return a + b;
+    }
 }
