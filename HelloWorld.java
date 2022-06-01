1e15f935247dc4ca53bbd18d279882e7d765033b
Koutli Maria <maria.koutli@pfizer.com>
2022-06-01 10:00:10 +0300

    clear data

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,15 +1,8 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        int a = 5;
-        int b = 2;
-        System.out.println(a + "+" + b + "=" + add(a,b));
+        System.out.println("Hello World");
     }
 
-    private static int add(int a, int b) {
-        return a+b;
-    }private int add10 (int a) {
-        return a + 10;
-    }
 
 }
