eb57c1188bebab06146fccbb53aa7e86c72c0b5f
Koutli Maria <maria.koutli@pfizer.com>
2022-06-09 13:36:33 +0300

    clear

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,18 +1,6 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        int a = 5;
-        int b = 2;
-        System.out.println(a + "+" + b + "=" + add(a,b));
-    }
-
-    private static int add(int a, int b) {
-        return a+b;
-    }
-    private int addConst (int a) {
-        return a + 10;
-    }
-    private static int minus(int a, int b) {
-        return a-b;
+        System.out.println("Hello World");
     }
 }
